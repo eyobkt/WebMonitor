77b170fb6e083e348d1dc7d07ad8fc30fe233dbe
eyobkt <et328@scarletmail.rutgers.edu>
2020-03-29 06:07:04 -0400

    Update MonitorServlet.java

--- src/MonitorServlet.java
+++ src/MonitorServlet.java
@@ -5,21 +5,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MonitorServlet extends HttpServlet {         
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-	    throws ServletException, IOException {
-	  String url = request.getParameter("url");	  
-	  String email = request.getParameter("email");
-	  
-	  DataAccessObject dao = new DataAccessObject();
-	  dao.insertMonitor(url, email);
-	  dao.closeConnection();
-	 
-	  response.setStatus(201);
-	}
-	
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) 
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) 
       throws ServletException, IOException {
-	  String url = request.getParameter("url");   
+    String url = request.getParameter("url");	  
+    String email = request.getParameter("email");
+	  
+    DataAccessObject dao = new DataAccessObject();
+    dao.insertMonitor(url, email);
+    dao.closeConnection();
+	 
+    response.setStatus(201);
+  }
+	
+  protected void doDelete(HttpServletRequest request, HttpServletResponse response) 
+      throws ServletException, IOException {
+    String url = request.getParameter("url");   
     String email = request.getParameter("email");
     
     DataAccessObject dao = new DataAccessObject();
